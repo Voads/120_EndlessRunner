7c8c72f82807294b0e5f3fae41954f4fca7bb7e8
Voads <blue.j.row@gmail.com>
2022-04-21 10:23:35 -0700

    created main.js

---
+++ src/main.js
@@ -0,0 +1,15 @@
+let config = {
+    type: Phaser.CANVAS,
+    width: 900,
+    height: 600,
+    //scene: [ Menu, Play ]
+}
+
+let game = new Phaser.Game(config);
+
+// set UI sizes
+// let borderUISize = game.config.height / 15;
+// let borderPadding = borderUISize / 3;
+
+// reserve keyboard variables
+let keyLEFT, keyRIGHT, keyUP, keyDOWN;
