3db72dd73180f1dc2eb17b794adefa48fb685d02
Keli Lindsey <kklindse@ucsc.edu>
2022-04-21 15:25:32 -0700

    updating Player.js

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -3,6 +3,6 @@ class Player extends Phaser.GameObjects.Sprite {
       super(scene, x, y, texture, frame);
   
       scene.add.existing(this); // add object to existing scene, displayList, updateList
-      this.moveSpeed = 2;       // pixels per frame
-      this.sfxRocket = scene.sound.add('sfx_rocket');   // add rocket sfx
+      this.moveSpeed = 5
+      ;
     }
