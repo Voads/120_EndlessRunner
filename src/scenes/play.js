f4f39728a80342ac4265110e92d1ee6d05af9ccc
Voads <blue.j.row@gmail.com>
2022-04-22 14:00:24 -0700

    fixed syntax errors (added this. a bunch of times)

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -22,16 +22,16 @@ class Play extends Phaser.Scene {
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);        
         
         // add player (p1)
-        player = this.physics.add.sprite(game.config.width/2 - 250, game.config.height - borderUISize - borderPadding - 353, 'player').setOrigin(0.5, 0);
-        player.setBounce(0.2);
-        player.setCollideWorldBounds(true);
+        this.player = this.physics.add.sprite(game.config.width/2 - 250, game.config.height - borderUISize - borderPadding - 353, 'player').setOrigin(0.5, 0);
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
 
         // add enemy 01
         this.enemy01 = new Spaceship(this, game.config.width/2 + 250, game.config.height - borderUISize - borderPadding - 353, 'enemy01').setOrigin(0.5, 0);
         
         // define keys
-        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-        keyJUMP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        this.keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.keyJUMP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         
         // initialize score
         this.p1Score = 0;
@@ -73,7 +73,8 @@ class Play extends Phaser.Scene {
  
         // update game objects
         if (!this.gameOver) {
-            this.p1Player.update();             // update player sprite
+            this.player.update();             // update player sprite
+            this.playerJump(this.player);
             this.enemy01.update();               // update enemy 01 sprite
         }   
  
@@ -85,12 +86,14 @@ class Play extends Phaser.Scene {
 
     checkCollision(player, enemy) {
         // simple AABB checking
-        if (player.x < enemy.x + enemy.width - 10 && 
-            player.x + player.width - 10 > enemy.x && 
-            player.y < enemy.y + enemy.height &&
-            player.height + player.y > enemy. y) {
-                return true;
-        } else {
+        if (this.player.x < this.enemy01.x + this.enemy01.width - 10 && 
+            this.player.x + this.player.width - 10 > this.enemy01.x && 
+            this.player.y < this.enemy01.y + this.enemy01.height &&
+            this.player.height + this.player.y > this.enemy01.y) 
+        {
+            return true;
+        } 
+        else {
             return false;
         }
     }
@@ -102,9 +105,10 @@ class Play extends Phaser.Scene {
     }
 
     playerJump(player) {
-        if (KeyF.isDown && player.body.touching.ground)
-{
-    player.setVelocityY(-330);
-}
+        if (this.keyJUMP.isDown) //player.body.touching.ground)
+        {
+            console.log("JUMPING...");
+            player.setVelocityY(-330);
+        }
     }
 }
